4f04f0ba6411d79bd51429a3758e0c51e83a2337
leonardo252 <leonardo.lol2525@gmail.com>
2017-10-17 11:54:55 -0300

    Add arquivos das aulas s03e02 e s03e03

--- hellojs-s03e02/index2.js
+++ hellojs-s03e02/index2.js
@@ -4,4 +4,24 @@ const api = axios.create({
   baseURL: "https://api.github.com"
 })
 
-api.get("/search/issues?q=S03E02T02 in:title+repo:/seita-ifce/hello-js-v3/+state:open&sort=created&order=asc").then(ret => console.log(ret.data))
+api.get("/search/issues?q=S03E02T02 in:title+state:open&sort=created&order=asc").then(ret => { 
+  const url = ret.data.items[0].comments_url;
+    api.get(url).then(ret =>{
+      const obj = ret.data;
+      let tam = obj.length;
+      for(let i =0; i<tam; i++){
+        let comment = obj[i].body;
+        let comper = comment.search("hellojs-s03e02");
+        if (comper !== -1){
+          let date = obj[i].created_at.slice(0,10);
+          if(date <= "2017-10-13"){
+            console.log("Nome: ", obj[i].user.login, "\nComentario : \n"+obj[i].body+"\n");
+
+          }
+        }
+      }
+    });
+})
+
+
+//
---
+++ hellojs-s03e03/hello-knex-1.js
@@ -0,0 +1,19 @@
+const knex = require("knex")({
+    client: "sqlite3",
+    connection: {
+      filename: "./hello.sqlite"
+    },
+    useNullAsDefault: true // coisas de sqlite.. ¯\_(ツ)_/¯
+  });
+  
+  // criar uma tabela
+knex.schema.createTable("convidado", (table) =>{
+    table.increments("idconvidado");
+    table.string("nomeconvidado");
+  }).then(() => {
+    console.log("tabela criada");
+    process.exit(0);
+  }).catch((err)=>{
+    console.log(err);
+    process.exit(1);
+  });
